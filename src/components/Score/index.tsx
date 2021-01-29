5609ce16b1e5e4081db6ea94d64db8f869b84cac
crayola-eater <a-sheikh@outlook.com>
2021-01-29 19:59:57 +0000

    Made Score heading's font size responsive

--- src/components/Score/index.tsx
+++ src/components/Score/index.tsx
@@ -8,7 +8,9 @@ const Score: React.FC<ScoreProps> = ({ players }) => {
 
   return (
     <div className="shadow-md p-2 md:p-4 flex flex-col justify-center items-center bg-gray-200 rounded-lg w-48 sm:w-72 md:w-96 space-y-2">
-      <h1 className="w-full text-xl text-center tracking-widest">Score</h1>
+      <h1 className="w-full text-sm sm:text-lg md:text-xl text-center tracking-widest">
+        Score
+      </h1>
       <table className="w-full border-collapse text-center bg-white">
         <thead>
           <tr>
