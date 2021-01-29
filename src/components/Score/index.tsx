5464431d5a42df3e675714766fc767efb68139af
crayola-eater <a-sheikh@outlook.com>
2021-01-29 19:58:40 +0000

    Fixed padding of outer div in Score

--- src/components/Score/index.tsx
+++ src/components/Score/index.tsx
@@ -7,7 +7,7 @@ const Score: React.FC<ScoreProps> = ({ players }) => {
   }
 
   return (
-    <div className="shadow-md p-4 flex flex-col justify-center items-center bg-gray-200 rounded-lg w-48 sm:w-72 md:w-96 space-y-2">
+    <div className="shadow-md p-2 md:p-4 flex flex-col justify-center items-center bg-gray-200 rounded-lg w-48 sm:w-72 md:w-96 space-y-2">
       <h1 className="w-full text-xl text-center tracking-widest">Score</h1>
       <table className="w-full border-collapse text-center bg-white">
         <thead>
