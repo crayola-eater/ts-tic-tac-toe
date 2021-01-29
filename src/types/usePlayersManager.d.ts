b4b07e32b964f8e9b35dcc92dc382e7b850a4058
crayola-eater <a-sheikh@outlook.com>
2021-01-29 19:21:17 +0000

    Added Score component

--- src/types/usePlayersManager.d.ts
+++ src/types/usePlayersManager.d.ts
@@ -1,6 +1,8 @@
 export interface Player {
+  index: number;
   icon: string;
   name: string;
+  score: number;
 }
 
 export interface PlayersManager {
@@ -9,6 +11,7 @@ export interface PlayersManager {
   currentPlayer: Player;
   addPlayer: (player: Player) => void;
   setNextPlayer: () => void;
+  incrementPlayerScore: (playerIndex: number, increment = 1) => void;
 }
 
 export type UsePlayersManager = () => PlayersManager;
