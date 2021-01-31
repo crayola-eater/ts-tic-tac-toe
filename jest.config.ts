71045736ca708f3be821deb8c6e0cd4994027b2a
crayola-eater <a-sheikh@outlook.com>
2021-01-31 15:09:54 +0000

    Enabled coverage option within Jest config file

--- jest.config.ts
+++ jest.config.ts
@@ -4,6 +4,7 @@ const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["./config/importJestDom.ts"],
+  collectCoverage: true,
 };
 
 export default config;
