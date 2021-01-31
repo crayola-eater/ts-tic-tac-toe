3d1cad94ae12bd2e18091773b7508512f547311b
crayola-eater <a-sheikh@outlook.com>
2021-01-31 12:22:34 +0000

    Changed Jest config file from JS to TS

--- jest.config.ts
+++ jest.config.ts
@@ -1,5 +1,9 @@
-module.exports = {
+import { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
   preset: "ts-jest",
   testEnvironment: "jsdom",
   setupFilesAfterEnv: ["./config/importJestDom.ts"],
 };
+
+export default config;
