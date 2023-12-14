e8d2d45d31b9444d9dda7fab3c87b581202c53b0
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-12-14 01:46:26 +0000

    Updates `usePlayersManager` tests to use Vitest and brings types closer

--- src/hooks/usePlayersManager.ts
+++ src/hooks/usePlayersManager.ts
@@ -1,41 +1,57 @@
-import { useState, useCallback } from "react";
-import { PlayersManager, UsePlayersManager } from "../types/usePlayersManager";
+import { useState, useCallback } from 'react';
 
-const usePlayersManager: UsePlayersManager = () => {
-  const [players, setPlayers] = useState<PlayersManager["players"]>([]);
+export type Player = {
+  index: number;
+  icon: string;
+  name: string;
+  score: number;
+};
+
+export type PlayersManager = {
+  players: Player[];
+  currentPlayerIndex: number;
+  currentPlayer: Player;
+  addPlayer: (player: Omit<Player, 'index' | 'score'>) => void;
+  setNextPlayer: () => void;
+  incrementPlayerScore: (playerIndex: number, increment?: number) => void;
+};
+
+export type AddPlayer = PlayersManager['addPlayer'];
+export type SetNextPlayer = PlayersManager['setNextPlayer'];
+export type IncrementPlayerScore = PlayersManager['incrementPlayerScore'];
+
+export default function usePlayersManager(): PlayersManager {
+  const [players, setPlayers] = useState<Player[]>([]);
   const [currentPlayerIndex, setCurrentPlayerIndex] = useState(0);
 
-  const addPlayer = useCallback<PlayersManager["addPlayer"]>(
-    (newPlayer) => {
-      setPlayers((prev) => [...prev, newPlayer]);
-    },
-    [setPlayers]
-  );
+  const addPlayer = useCallback<AddPlayer>((options) => {
+    setPlayers((prev) => {
+      const newPlayer: Player = {
+        ...options,
+        index: prev.length,
+        score: 0,
+      };
+      return [...prev, newPlayer];
+    });
+  }, []);
 
-  const setNextPlayer = useCallback<PlayersManager["setNextPlayer"]>(() => {
+  const setNextPlayer = useCallback<SetNextPlayer>(() => {
     if (0 === players.length) {
       return;
     }
     setCurrentPlayerIndex((prev) => (prev + 1) % players.length);
-  }, [setCurrentPlayerIndex, players]);
+  }, [players]);
 
-  const incrementPlayerScore = useCallback<
-    PlayersManager["incrementPlayerScore"]
-  >(
-    (playerIndex, increment = 1) => {
-      setPlayers((prev) => {
-        return [
-          ...prev.slice(0, playerIndex),
-          {
-            ...prev[playerIndex],
-            score: prev[playerIndex].score + increment,
-          },
-          ...prev.slice(playerIndex + 1),
-        ];
-      });
-    },
-    [setPlayers]
-  );
+  const incrementPlayerScore = useCallback<IncrementPlayerScore>((playerIndex, increment = 1) => {
+    setPlayers((prev) => {
+      const updated = {
+        ...prev[playerIndex],
+        score: prev[playerIndex].score + increment,
+      };
+
+      return prev.with(playerIndex, updated);
+    });
+  }, []);
 
   return {
     players,
@@ -45,6 +61,4 @@ const usePlayersManager: UsePlayersManager = () => {
     setNextPlayer,
     incrementPlayerScore,
   };
-};
-
-export default usePlayersManager;
+}
