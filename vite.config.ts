a4b78f26ee0c0a1676696e5f4c54a82541112209
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-12-14 00:50:25 +0000

    Migrates project from Webpack to Vite

---
+++ vite.config.ts
@@ -0,0 +1,17 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react';
+import tailwindcss from 'tailwindcss';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  css: {
+    postcss: {
+      plugins: [tailwindcss()],
+    },
+  },
+  test: {
+    environment: 'jsdom',
+    setupFiles: './src/test-setup.ts',
+  },
+});
