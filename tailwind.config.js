9eb21bf7840e00ce0df115927fd2bac9c045a7b3
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-12-14 01:47:38 +0000

    Removes files from old project structure

--- tailwind.config.js
+++
@@ -1,8 +0,0 @@
-/** @type {import('tailwindcss/tailwind-config').TailwindConfig} */
-module.exports = {
-  content: ["./src/**/*.{html,js,jsx,ts,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-};
