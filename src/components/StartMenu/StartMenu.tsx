4a351826beaff8cd4b3d6ed96315059ed79eef0d
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-12-14 01:38:17 +0000

    Updates `StartMenu` component to use Vitest and new project structure

--- src/components/StartMenu/StartMenu.tsx
+++ src/components/StartMenu/StartMenu.tsx
@@ -1,47 +1,54 @@
-import React from "react";
-import { useForm } from "react-hook-form";
-import { StartMenuFormData, StartMenuProps } from "../../types/StartMenu";
+import { useForm } from 'react-hook-form';
 
-const allIcons = [
-  "🧟",
-  "☠️",
-  "⚰️",
-  "🎃",
-  "👻",
-  "👹",
-  "🤡",
-  "💀",
-  "👾",
-  "👺",
-  "👽",
-  "🧙",
-  "🧛",
-  "👸",
-  "🧡",
-].map((icon, i) => {
+export type StartMenuProps = {
+  handleStart: (data: StartMenuFormData) => void;
+};
+
+export type StartMenuFormData = {
+  player1Name: string;
+  player1Icon: string;
+  player2Name: string;
+  player2Icon: string;
+};
+
+const allIcons = (
+  [
+    '🧟',
+    '☠️',
+    '⚰️',
+    '🎃',
+    '👻',
+    '👹',
+    '🤡',
+    '💀',
+    '👾',
+    '👺',
+    '👽',
+    '🧙',
+    '🧛',
+    '👸',
+    '🧡',
+  ] as const
+).map((icon, i) => {
   return {
     icon,
     id: i,
   };
 });
 
-const StartMenu: React.FC<StartMenuProps> = ({ handleStart }) => {
+export default function StartMenu({ handleStart }: StartMenuProps) {
   const { register, handleSubmit } = useForm<StartMenuFormData>();
 
   return (
     <>
-      <h1 className="text-center text-3xl p-2 tracking-wider">
-        Select your icon!
-      </h1>
+      <h1 className="text-center text-3xl p-2 tracking-wider">Select your icon!</h1>
       <form
         className="flex flex-col justify-center items-center space-y-3 w-11/12 sm:w-96 tracking-wider"
         onSubmit={handleSubmit(handleStart)}
       >
         {Array.from({ length: 2 }, (_, i) => {
-          const nameId =
-            i === 0 ? ("player1Name" as const) : ("player2Name" as const);
-          const iconId =
-            i === 0 ? ("player1Icon" as const) : ("player2Icon" as const);
+          const nameId = i === 0 ? 'player1Name' : 'player2Name';
+          const iconId = i === 0 ? 'player1Icon' : 'player2Icon';
           return (
             <fieldset
               className="w-full flex flex-col justify-center items-center rounded-lg bg-gray-200 p-4 space-y-3"
@@ -54,13 +61,11 @@ const StartMenu: React.FC<StartMenuProps> = ({ handleStart }) => {
                 className="w-full flex flex-col justify-center items-center space-y-1"
                 htmlFor={nameId}
               >
-                <span className="w-full text-gray-600 text-xs">{`Player ${
-                  i + 1
-                } name:`}</span>
+                <span className="w-full text-gray-600 text-xs">{`Player ${i + 1} name:`}</span>
                 <input
                   className="w-full rounded-sm p-1"
                   {...register(nameId, {
-                    required: "Please enter a name",
+                    required: 'Please enter a name',
                     pattern: /\S+/,
                   })}
                   type="text"
@@ -72,9 +77,7 @@ const StartMenu: React.FC<StartMenuProps> = ({ handleStart }) => {
                 className="w-full flex flex-col justify-center items-center space-y-1"
                 htmlFor={iconId}
               >
-                <span className="w-full text-gray-600 text-xs">{`Player ${
-                  i + 1
-                } icon:`}</span>
+                <span className="w-full text-gray-600 text-xs">{`Player ${i + 1} icon:`}</span>
                 <select
                   className="w-full rounded-sm p-1"
                   id={iconId}
@@ -98,12 +101,10 @@ const StartMenu: React.FC<StartMenuProps> = ({ handleStart }) => {
             </fieldset>
           );
         })}
-        <button className="bg-gray-200 hover:bg-gray-400 hover:bg-green-700 hover:text-white p-3 rounded-md tracking-wider text-md">
+        <button className="bg-gray-200 hover:bg-green-700 hover:text-white p-3 rounded-md tracking-wider text-md">
           Start game
         </button>
       </form>
     </>
   );
-};
-
-export default StartMenu;
+}
