3960062a8eedef131b3d70961e6c396c14e3c8d9
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-12-16 14:43:59 +0000

    Simplifies `App` component

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,6 +1,5 @@
-import useTicTacToe from '../../hooks/useTicTacToe';
+import useTicTacToe from '../../useTicTacToe/useTicTacToe';
 import Board from '../Board/Board';
-import ResponsiveWrapper from '../ResponsiveWrapper/ResponsiveWrapper';
 import Score from '../Score/Score';
 import StartMenu from '../StartMenu/StartMenu';
 import Status from '../Status/Status';
@@ -8,26 +7,36 @@ import Status from '../Status/Status';
 export default function App() {
   const ticTacToe = useTicTacToe();
 
+  let children = null;
+
+  if (ticTacToe.gameStatus === 'NOT_STARTED') {
+    children = <StartMenu handleStart={ticTacToe.handlers.handleStart} />;
+  } else if (ticTacToe.players) {
+    children = (
+      <>
+        <Status
+          currentPlayer={ticTacToe.currentPlayer}
+          status={ticTacToe.gameStatus}
+          winner={ticTacToe.winner}
+        />
+        <Board
+          board={ticTacToe.board}
+          currentPlayer={ticTacToe.currentPlayer}
+          status={ticTacToe.gameStatus}
+          handleMove={ticTacToe.handlers.handleMove}
+        />
+        <Score players={ticTacToe.players} />
+      </>
+    );
+  } else {
+    children = <p>Loading the game...</p>;
+  }
+
   return (
-    <ResponsiveWrapper>
-      {0 === ticTacToe.playersManager.players.length ? (
-        <StartMenu handleStart={ticTacToe.handlers.handleStart} />
-      ) : (
-        <>
-          <Status
-            currentPlayer={ticTacToe.playersManager.currentPlayer}
-            gameHasFinished={ticTacToe.gameManager.gameHasFinished}
-            winner={ticTacToe.gameManager.winner}
-          />
-          <Board
-            board={ticTacToe.boardManager.board}
-            currentPlayer={ticTacToe.playersManager.currentPlayer}
-            gameHasFinished={ticTacToe.gameManager.gameHasFinished}
-            handleMove={ticTacToe.handlers.handleMove}
-          />
-          <Score players={ticTacToe.playersManager.players} />
-        </>
-      )}
-    </ResponsiveWrapper>
+    <div className="flex flex-col justify-center items-center h-screen w-screen">
+      <main className="grid flex-col justify-center w-11/12 max-w-max space-y-3 place-items-center">
+        {children}
+      </main>
+    </div>
   );
 }
