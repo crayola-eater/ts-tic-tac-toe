8eeb0c60519b0c412070cb4ab887b1efb5a5372a
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-12-16 14:45:27 +0000

    Simplifies `Score` component and tests

--- src/components/Score/Score.test.tsx
+++ src/components/Score/Score.test.tsx
@@ -3,47 +3,38 @@ import { render, screen } from '@testing-library/react';
 import Score from './Score';
 
 describe('Score component', () => {
-  const players = [
-    {
-      index: 0,
-      icon: '⚰️',
-      name: 'APASDJPASD',
-      score: 4,
-    },
-    {
-      index: 1,
-      icon: '👽',
-      name: 'TASDJPAOS',
-      score: 7,
-    },
-  ];
+  const player1 = {
+    index: 0,
+    icon: '⚰️',
+    name: 'APASDJPASD',
+    score: 4,
+  };
+
+  const player2 = {
+    index: 1,
+    icon: '👽',
+    name: 'TASDJPAOS',
+    score: 7,
+  };
 
   it('should display the heading', async () => {
-    render(<Score players={players} />);
+    render(<Score players={[player1, player2]} />);
     const heading = await screen.findByRole('heading', { name: 'Score' });
     expect(heading).toBeInTheDocument();
   });
 
   it('should display all players and scores in correct order', async () => {
-    render(<Score players={players} />);
+    render(<Score players={[player1, player2]} />);
 
     const tableHeaders = await screen.findAllByRole('columnheader');
-    expect(tableHeaders).to.have.length(players.length);
+    expect(tableHeaders).to.have.length(2);
 
     const tableCells = await screen.findAllByRole('cell');
-    expect(tableCells).to.have.length(players.length);
+    expect(tableCells).to.have.length(2);
 
-    for (const [i, player] of players.entries()) {
+    for (const [i, player] of [player1, player2].entries()) {
       expect(tableHeaders[i]).toHaveTextContent(`${player.name} ${player.icon}`);
       expect(tableCells[i]).toHaveTextContent(player.score.toString(10));
     }
-  });
-
-  it('should render nothing if there are no players', async () => {
-    const { container } = render(<Score players={[]} />);
-    expect(container).toBeEmptyDOMElement();
-
-    const heading = screen.queryByRole('heading', { name: 'Score' });
-    expect(heading).toBeNull();
   });
 });
