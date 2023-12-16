8e93ae70c060d9669403489ce3aa8be9d2078f68
crayola-eater <67001073+crayola-eater@users.noreply.github.com>
2023-12-16 14:45:02 +0000

    Simplifies `BoardSquare` component and tests

--- src/components/BoardSquare/BoardSquare.tsx
+++ src/components/BoardSquare/BoardSquare.tsx
@@ -1,28 +1,34 @@
 import clsx from 'clsx';
-import type { BoardSquare } from '../../hooks/useBoardManager';
-import type { TicTacToe } from '../../hooks/useTicTacToe';
+import type { GameStatus, BoardSquare } from '../../useTicTacToe/types';
 
 export type BoardSquareProps = {
   square: BoardSquare;
   handleClick: () => void;
-  gameHasFinished: TicTacToe['gameManager']['gameHasFinished'];
+  status: GameStatus;
 };
 
-export default function BoardSquare({ square, handleClick, gameHasFinished }: BoardSquareProps) {
+export default function BoardSquare({ square, handleClick, status }: BoardSquareProps) {
   const className = clsx(
     'shadow-md',
     'rounded',
     'flex flex-col justify-center items-center',
     'border border-gray-500 border-solid',
     'transition-colors duration-200 ease-linear',
-    { ['hover:bg-gray-200']: !gameHasFinished },
-    { ['bg-gray-200']: square.isOccupied },
-    { ['bg-yellow-500']: square.isWinning },
-    { ['bg-gray-200 opacity-40']: gameHasFinished && !square.isWinning },
+    {
+      ['hover:bg-gray-200']: status !== 'FINISHED',
+      ['bg-gray-200']: square.isOccupied,
+      ['bg-yellow-500']: square.isWinning,
+      ['bg-gray-200 opacity-40']: status === 'FINISHED' && !square.isWinning,
+    },
   );
 
   return (
-    <button className={className} onClick={handleClick} disabled={gameHasFinished}>
+    <button
+      className={className}
+      onClick={status === 'IN_PROGRESS' && !square.isOccupied ? handleClick : undefined}
+      disabled={status === 'FINISHED'}
+    >
+      <h1 className="bg"></h1>
       <span
         className={clsx('text-3xl md:text-4xl lg:text-5xl', {
           ['animate-bounce']: square.isWinning,
